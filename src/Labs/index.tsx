8d2502bc6fe0734792346cda3b26f0d64f5e4987
nicoler717 <rosas.n@northeastern.edu>
2024-03-19 13:58:08 -0400

    lab of a4

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,20 +1,21 @@
-import Assignment3 from "./a3";
 import Nav from "../Nav";
-import { Link } from "react-router-dom";
-import { Route, Routes } from "react-router";
-
+import Assignment3 from "./a3";
+import Assignment4 from "./a4";
+import { Routes, Route, Navigate } from "react-router";
+import store from "./store";
+import { Provider } from "react-redux";
 function Labs() {
   return (
-    <div className="container-fluid">
-      <h1>Labs</h1>
-      <Nav />
-      <Link to="/Labs/a3">Assignment 3</Link> |
-      {/*<Link to="/Labs/a3">Assignment 4</Link>*/}
-      <Routes>
-        <Route path="/a3/*" element={<Assignment3 />} />
-        {/*<Route path="/a3" element={<Assignment4 />} />*/}
-      </Routes>
-    </div>
+    <Provider store={store}>
+      <div>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Navigate to="a3" />} />
+          <Route path="a3" element={<Assignment3 />} />
+          <Route path="a4" element={<Assignment4 />} />
+        </Routes>
+      </div>
+    </Provider>
   );
 }
 export default Labs;
--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,25 +1,19 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 function Nav() {
-  const { pathname } = useLocation();
   return (
-    <nav className="nav nav-tabs mt-2">
-      <Link
-        to="/Labs/a3"
-        className={`nav-link ${pathname.includes("a3") ? "active" : ""}`}
-      >
+    <nav className="nav nav-pills mb-2">
+      <Link className="nav-link" to="/Labs/a3">
         A3
       </Link>
-      <Link
-        to="/Kanbas"
-        className={`nav-link ${pathname.includes("Kanbas") ? "active" : ""}`}
-      >
-        Kanbas
+      <Link className="nav-link" to="/Labs/a4">
+        A4
       </Link>
-      <Link
-        to="/hello"
-        className={`nav-link ${pathname.includes("hello") ? "active" : ""}`}
-      >
+      <Link className="nav-link" to="/hello">
         Hello
+      </Link>
+      <Link className="nav-link" to="/Kanbas">
+        Kanbas
       </Link>
     </nav>
   );
